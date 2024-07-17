12f79cf89aef6ade696207441ef5e29862750fe5
flurl <florian.st.klug@gmail.com>
2024-07-17 11:25:24 +0200

    Rename project

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import secret
 
-APP_NAME = "S.H.I.C.E - Super Helper Identifying Cat Entities"
+APP_NAME = "S.H.I.C.E - Sophisticated Helper Identifying Cat Entities"
 
 CAPTURED_IMG_PATH = "captured_images/"
 TAGGED_PATH = "tagged/"
